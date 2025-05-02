dc9932249b101849746f7d4f2ee2d73983749592
douglasdans <douglas.dans@gmail.com>
2025-05-02 18:25:08 -0300

    feat: adiciona configuração de Docker para os serviços bus-server e bus-api

---
+++ bus-api/cole-isso-na-node-modules/bus-promise/build/server/constants/docker-api.js
@@ -0,0 +1,91 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+exports.default = {
+  sptrans: "http://api.olhovivo.sptrans.com.br/v2.1",
+  server: "http://server:8888",
+  auth: {
+    route: "/login/autenticar",
+    required: ["token"],
+  },
+  lines: {
+    route: "/linha/buscar",
+    required: ["terms"],
+    proxyParams: {
+      terms: "termosBusca",
+    },
+  },
+  shapes: {
+    route: "/shapes",
+    required: ["shapeId"],
+    proxyParams: {
+      shapeId: "codigoTrajeto",
+    },
+  },
+  stops: {
+    route: "/parada/buscar",
+    required: ["terms"],
+    proxyParams: {
+      terms: "termosBusca",
+    },
+  },
+  stopsByLine: {
+    route: "/parada/buscarParadasPorLinha",
+    required: ["lineId"],
+    proxyParams: {
+      lineId: "codigoLinha",
+    },
+  },
+  corridors: {
+    route: "/corredor",
+  },
+  stopsByCorridor: {
+    route: "/parada/buscarParadasPorCorredor",
+    required: ["corridorId"],
+    proxyParams: {
+      corridorId: "codigoCorredor",
+    },
+  },
+  vehiclesPosition: {
+    route: "/posicao/linha",
+    required: ["lineId"],
+    proxyParams: {
+      lineId: "codigoLinha",
+    },
+  },
+  arrivalForecast: {
+    route: "/previsao",
+    required: ["stopId", "lineId"],
+    proxyParams: {
+      stopId: "codigoParada",
+      lineId: "codigoLinha",
+    },
+  },
+  lineForecast: {
+    route: "/previsao/linha",
+    required: ["lineId"],
+    proxyParams: {
+      lineId: "codigoLinha",
+    },
+  },
+  stopForecast: {
+    route: "/previsao/parada",
+    required: ["stopId"],
+    proxyParams: {
+      stopId: "codigoParada",
+    },
+  },
+  linesDirection: {
+    route: "/linha/buscarLinhaSentido",
+    required: ["terms", "direction"],
+    proxyParams: {
+      terms: "termosBusca",
+      direction: "sentido",
+    },
+  },
+  companies: {
+    route: "/empresa",
+  },
+};
